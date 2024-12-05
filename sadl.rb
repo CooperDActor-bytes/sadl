24a040d975b729e557e1e85a29d63735ec2602a7
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-12-05 23:28:00 +1100

    Update sadl.rb

--- sadl.rb
+++ sadl.rb
@@ -1,7 +1,7 @@
 class Sadl < Formula
   desc "saltymedia downloader in rust"
   homepage "https://github.com/CooperDActor-bytes/sadl"
-  url "https://github.com/CooperDActor-bytes/sadl/archive/v1.0.0.tar.gz"
+  url "https://github.com/CooperDActor-bytes/sadl/archive/v1.0.1.tar.gz"
   sha256 "a1140e2117549c19fb063cf2d0dbf26677145d8fff83731051b9b1bee140270f"
 
   depends_on "rust" => :build
