3db804b859bc013eec1b459f2290d8ccfd1e5605
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-12-05 22:57:09 +1100

    Update sadl.rb

--- sadl.rb
+++ sadl.rb
@@ -1,21 +1,18 @@
 class Sadl < Formula
-  desc "saltymedia downloader - instagram"
-  homepage "sadl.saltymedia.stream"
-  url "https://github.com/yourusername/sadl/archive/v1.0.0.tar.gz"
-  sha256 "your_tarball_sha256"
-  license "MIT"
+  desc "saltymedia downloader in rust"
+  homepage "https://github.com/CooperDActor-bytes/sadl"
+  url "https://github.com/CooperDActor-bytes/sadl/archive/v1.0.0.tar.gz"
+  sha256 "your-archive-sha256-checksum"
 
-  depends_on "python@3.11"
-  depends_on "ffmpeg"
+  depends_on "rust" => :build
 
   def install
-    bin.install "sadl"
-    system "pip3", "install", "yt-dlp"
+    system "cargo", "build", "--release"
+    bin.install "target/release/sadl"
+    man1.install "man/sadl.1"
   end
 
   test do
-    (testpath/"insta.txt").write "https://www.instagram.com/reel/abc123/"
-    system "#{bin}/sadl", "-I", "insta.txt"
+    system "#{bin}/sadl", "--version"
   end
 end
-
